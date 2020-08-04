60c544f7deb7c38892dfd99d921da9497f37d229
Balázs Varga <balazsvarga91@gmail.com>
2020-08-04 21:28:23 +0200

    add login page css

--- with-frontend/src/app/auth/auth.module.ts
+++ with-frontend/src/app/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
@@ -11,7 +11,7 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   declarations: [
-    LoginComponent,
+    LoginComponent
   ]
 })
 export class AuthModule { }
