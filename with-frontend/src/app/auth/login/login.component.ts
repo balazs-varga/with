a5a94d02c11599cdb4b05e7efb751dad5a530dc7
Balázs Varga <balazsvarga91@gmail.com>
2020-08-06 20:39:18 +0200

    display form error messages on login page

--- with-frontend/src/app/auth/login/login.component.ts
+++ with-frontend/src/app/auth/login/login.component.ts
@@ -30,7 +30,7 @@ export class LoginComponent implements OnInit {
   // use password regex
   private createLoginForm(): void {
     this.loginForm = this.fb.group({
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
       password: ['', Validators.required]
     });
   }
