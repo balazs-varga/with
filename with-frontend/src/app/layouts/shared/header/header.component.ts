c4d559dd003f52d3cfb85bb1639be189b4008757
Balázs Varga <balazsvarga91@gmail.com>
2020-09-24 20:52:11 +0200

    logout user

--- with-frontend/src/app/layouts/shared/header/header.component.ts
+++ with-frontend/src/app/layouts/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public auth: AuthenticationService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    this.auth.logout().subscribe(() => {
+      this.router.navigateByUrl('/login');
+    });
+  }
 }
