31c79b0edcc9fbd7cfd6f6ed4ca0984796aa39b5
Balázs Varga <balazsvarga91@gmail.com>
2020-11-24 18:38:33 +0100

    display restaurants

--- with-frontend/src/app/restaurants/restaurant-list/restaurants.component.ts
+++ with-frontend/src/app/restaurants/restaurant-list/restaurants.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RestaurantsCommunicationService } from '../restaurants.communication.service';
 
 @Component({
@@ -11,9 +12,13 @@ export class RestaurantsComponent implements OnInit, OnDestroy {
   restaurants = [];
 
   constructor(
-    private restaurantsCommunicationService: RestaurantsCommunicationService
+    private restaurantsCommunicationService: RestaurantsCommunicationService,
+    private router: Router
   ) {
     this.restaurants = this.restaurantsCommunicationService.restaurants;
+    if (!this.restaurants) {
+      this.router.navigateByUrl('');
+    }
   }
 
   ngOnInit(): void {
@@ -22,5 +27,4 @@ export class RestaurantsComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.restaurantsCommunicationService.restaurants = [];
   }
-
 }
